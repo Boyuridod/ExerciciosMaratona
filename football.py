48208dc2b38d9b39512549814c2303a1b0cca945
Yuri David Silva Duarte <yuridsd@hotmail.com>
2025-06-02 11:39:15 -0300

    Update football.py

--- football.py
+++ football.py
@@ -8,17 +8,22 @@ contZero = 0
 maiorZero = 0
 
 for i in jogadores:
-    if i == '1':
+    if(i == '1'):
         cont += 1
-        if(contZero > maiorZero):
-            maiorZero = contZero
-    else:
         if(cont > maior):
             maior = cont
-        cont = 0
-        contZero += 1
 
-if(maior > 7 or maiorZero > 7):
+        contZero = 0
+    else:
+        contZero += 1
+        if(contZero > maiorZero):
+            maiorZero = contZero
+
+        cont = 0
+
+# print(contZero, maiorZero, cont, maior)
+
+if(maior >= 7 or maiorZero >= 7):
     print("YES")
 else:
     print("NO")
