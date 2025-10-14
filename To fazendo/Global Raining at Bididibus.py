bdf2d5270ca745e6d1c25a09afc5391d3fc5d97c
Yuri David Silva Duarte <102871804+Boyuridod@users.noreply.github.com>
2025-10-14 12:00:36 -0300

    Update Global Raining at Bididibus.py

--- To fazendo/Global Raining at Bididibus.py
+++ To fazendo/Global Raining at Bididibus.py
@@ -5,38 +5,12 @@ import re
 def exercicio():
     relevo = list(input())
 
-    cont = 0
-
-    for i in range(len(relevo) - 1, 0, -1):
-        if(relevo[i] == "/"):
-            cont = i
-            break
-
-    relevo = relevo[0: cont]
-
-    while(True):
-        try:
-            relevo.pop(relevo.index("/"))
-        except:
-            break
-
-    area = 0
-    cont = 0
     i = 0
-    lenrelevo = len(relevo)
+    profundidade = 0
     
-    while(i < lenrelevo):
-        if(relevo[i] == '\\'):
-            cont += 1
-
-        else:
-            area += cont
-            relevo.pop(i)
-            lenrelevo -= 1
+    while(i < len(relevo)):
 
         i += 1
-
-    area += len(relevo)
 
     print(area)
 
