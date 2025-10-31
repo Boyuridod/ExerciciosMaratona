cd50912c8a8974ce8e1c3624a602541ccdc1203a
Yuri David Silva Duarte <102871804+Boyuridod@users.noreply.github.com>
2025-10-31 11:47:10 -0300

    Update Lecographically Maximum.py

--- To fazendo/Deixei pra depois/Lecographically Maximum.py
+++ To fazendo/Deixei pra depois/Lecographically Maximum.py
@@ -1,35 +1,14 @@
 # https://vjudge.net/contest/747064#problem/L
 
-# TLE
-
 N = int(input())
 a = list(map(int, input().split(" ")))
 
 a.sort(reverse=True)
+res = []
 
-for i in range(len(a)):
-    a[i] = list(map(int, format(a[i], "b")))
+for i in range(N):
+    a[i] &= sum(a) - a[i] - sum(res)
+    res.append(a[i])
+    a[i] = 0
 
-for i in a:
-    for j in range(len(a[0]) - len(i)):
-        i.insert(0, 0)
-
-for i in range(len(a[0])):
-    qtt = 0
-    for j in range(len(a)):
-        if(a[j][i] == 1):
-            qtt += 1
-    for j in range(len(a)):
-        if(qtt > 0):
-            a[j][i] = 1
-            qtt -= 1
-        else:
-            a[j][i] = 0
-
-for i in a:
-    aux = "".join(map(str, i))
-    aux = int(aux, 2)
-
-    print(aux, end=" ")
-
-print("")
+print(*res)
